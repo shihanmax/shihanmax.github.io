9c427b1c295880b76f069f52bd6d5e1b9a369b38
shihanmax <ma_xan@126.com>
2021-04-04 01:47:32 +0800

    [foo]

--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 bundle exec jekyll build
-bundle exec jekyll serve --host 0.0.0.0 --port 80 --watch
+bundle exec jekyll serve --host 0.0.0.0 --port 80 --watch --force_polling --incremental
