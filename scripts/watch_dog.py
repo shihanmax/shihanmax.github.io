12a80ea6f6c960e2754579642030fa40279a97fd
Mashihan <shihanmax@foxmail.com>
2022-12-29 16:11:46 +0800

    foo

--- scripts/watch_dog.py
+++ scripts/watch_dog.py
@@ -6,17 +6,14 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.route("/hook", methods=['POST'])
-def add():
-    print(request.form) 
-    query = request.form["params"].strip()
-    return {"status": "ok", "msg": "success"}
+def hook():
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     app.run(debug=True, port=8082)
