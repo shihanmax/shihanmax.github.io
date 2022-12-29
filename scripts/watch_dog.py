5b1e6462ed77745f3649f16e86c2112107d2e3cf
Mashihan <shihanmax@foxmail.com>
2022-12-29 16:15:50 +0800

    foo

--- scripts/watch_dog.py
+++ scripts/watch_dog.py
@@ -1,11 +1,10 @@
-import os
-import sys
-import re
-import time
 import logging
+import os
+import time
 
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 from flask import Flask
 
@@ -15,9 +14,9 @@ app = Flask(__name__)
 @app.route("/hook", methods=['POST'])
 def hook():
     os.system("git pull")
-    print(f"Update at {time.ctime()}")
+    logger.info(f"Update at {time.ctime()}")
     return {"status": "ok"}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=8082)
+    app.run(host="0.0.0.0", port=8082)
